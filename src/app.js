eb0c6cd4f50513bb2a4aeb52113fe1dd25463b40
poonam khandelwal <poonamkh01@gmail.com>
2024-08-21 10:16:22 +0530

    hbs connected

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app=express()
 const dotenv=require('dotenv')
 dotenv.config({path:'./.env'})
 
-
+const hbs =require('hbs')
 
 const path=require('path')
 const userRouter=require('./routes/userRoute.js')
@@ -19,12 +19,22 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true})) 
 app.use(cookieParser())
+const static_path=path.join(__dirname,'/pages')
+const template_path=path.join(__dirname,'/views/templates')
+const partials_path=path.join(__dirname,'/views/partials')
+
+app.use(express.static(static_path))
+app.set('view engine','hbs')
+app.set('views',template_path)
+
+hbs.registerPartials(partials_path)
 
 app.use('/api/item',itemRouter)
 app.use("/api/user",userRouter) 
 
 app.get('/',(req,res)=>{
-    res.send('helloo from deployment')
+    // res.send('helloo from deployment')
+    res.render('login')
 })
 
 app.listen(port,()=>{
